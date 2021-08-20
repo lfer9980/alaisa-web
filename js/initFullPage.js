d4ee5cfa53c5e6ad1814259cd0ef9f95db6a8ba4
lfer9980 <luis_fernandez99@hotmail.com>
2021-08-20 13:34:43 -0600

    scrolling transition to index.html done, be careful compiling header.pug 08/20/2021

---
+++ js/initFullPage.js
@@ -0,0 +1,12 @@
+new fullpage('#fullpage', {
+	//options here
+	anchors:['1', '2', '3', '4', '5','6','7'],
+	autoScrolling:true,
+	keyboardScrolling: true,
+	verticalCentered: false,
+	scrollingSpeed: 1000,
+	menu: '#myMenu',
+});
+
+//métodos
+fullpage_api.setAllowScrolling(true);
