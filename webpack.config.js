c0ba450e1e92b20600945fb48f1c5b7249519486
lfer9980 <luis_fernandez99@hotmail.com>
2022-01-29 21:34:03 -0700

    :art: [MODIFIED] modificada la arquitectura de la app
    añadidas nuevas carpetas y configurando webpack

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[contenthash].js',
+		assetModuleFilename: 'assets/images/[hash][ext][query]',
+		publicPath: '/',
+	},
+	mode: 'production',
+	resolve: {
+		extensions: ['.js', '.jsx'],
+		alias: {
+			'@icons': path.resolve(__dirname, 'src/assets/icons'),
+			'@images': path.resolve(__dirname, 'src/assets/images'),
+			'@fonts': path.resolve(__dirname, 'src/assets/fonts'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@containers': path.resolve(__dirname, 'src/containers'),
+			'@context': path.resolve(__dirname, 'src/context'),
+			'@hooks': path.resolve(__dirname, 'src/hooks'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@routes': path.resolve(__dirname, 'src/routes'),
+			'@scripts': path.resolve(__dirname, 'src/scripts'),
+			'@styles': path.resolve(__dirname, 'src/styles'),
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.css|.styles|.scss$/i,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: 'asset'
+			},
+			{
+				test: /\.(woff|woff2)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: "assets/fonts//[name][contenthash].[ext]"
+				}
+			}
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: 'body',
+			template: 'public/index.html',
+			filename: './index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'assets/[name].[contenthash].css'
+		}),
+		new Dotenv(),
+		new CleanWebpackPlugin()
+	],
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new CssMinimizerPlugin(),
+			new TerserPlugin(),
+		],
+	}
+}
