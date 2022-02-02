106c4c49191ff6e84f6ed87ad97494ce41b52f2a
lfer9980 <luis_fernandez99@hotmail.com>
2022-02-01 20:38:08 -0700

    :art: [ADD] añadido ultimo componente principal

--- src/pages/Publicaciones.jsx
+++ src/pages/Publicaciones.jsx
@@ -1,11 +1,44 @@
 import React from "react";
 import { useDocumentTitle } from "@hooks/useDocumentTitle";
+import { Footer } from "@components/Footer";
 
 function Publicaciones() {
+	useDocumentTitle("Publicaciones")
 	return(
-		<section>
-			<h2>Publicaciones</h2>
-		</section>
+		<> 
+			<main className="publicaciones">
+				<section className="publicaciones__year">
+					<h4 className="title--blue">
+						2022
+					</h4>
+					<div className="publicaciones__content">
+						<span>
+							Referencia a publicacion 1,
+						</span>
+						<p>
+							breve descripción de la publicación para llegar al link de la publicacion
+						</p>
+						<hr /> 
+					</div>
+				</section>
+				<section className="publicaciones__updates">
+					<h4 className="title--blue">
+						v1.0.0
+					</h4>
+					<div className="publicaciones__content">
+						<span>
+							Referencia a publicacion 1,
+						</span>
+						<p>
+							breve descripción de la publicación para llegar al link de la publicacion
+						</p>
+						<hr /> 
+					</div>
+				</section>
+			</main>
+			<Footer />
+		</>
+
 	);
 }
 
